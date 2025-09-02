a0aa2a822f5d5b87a252c274e71d685dfd46dbd4
Mariano López <marlobyte.coder@gmail.com>
2025-09-02 14:46:37 -0300

    feat: integrar Pinia en main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './core/router'
 import vuetify from './core/plugins/vuetify'  
-import './core/plugins/webfontloader'        
+import './core/plugins/webfontloader'
+
+// Crear instancia de Pinia
+const pinia = createPinia()
 
 const app = createApp(App)
+app.use(pinia)    // Agregue Pinia ANTES del router
 app.use(router)
 app.use(vuetify)
 app.mount('#app')
